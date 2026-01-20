a379722e589b3d929b0dce3263ed61894e15ac41
Oscar <ogge9909vs@hotmail.com>
2026-01-20 12:05:19 +0100

    changes

---
+++ jvscrp/main.js
@@ -0,0 +1,83 @@
+
+/*const age = 25;
+if (age >= 18) {
+    console.log('You are an adult.');
+} else {
+    console.log('You are a minor.');
+}
+
+for (let i = 0; i < 5; i++) {
+    console.log('Iteration number: ' + i);
+}
+function greet(name) {
+    return 'Hello, ' + name + '!';
+}*/
+
+/*function andFunktion(a, b) {               //AND Funktion
+    return a === true && b === true;
+}
+console.log(andFunktion(false, false)); // Output: false
+console.log(andFunktion(false, true));  // Output: false
+console.log(andFunktion(true, false)); // Output: false
+console.log(andFunktion(true, true));  // Output: true */
+
+
+/*function orFunktion(a, b) {           //OR Funktion
+    return a === true || b === true;
+}
+console.log(orFunktion(false, false)); // Output: false
+console.log(orFunktion(false, true));  // Output: true
+console.log(orFunktion(true, false)); // Output: true
+console.log(orFunktion(true, true));  // Output: true
+
+function notFunktion(a) {         //NOT Funktion
+    return a !== true;
+}  
+console.log(notFunktion(false)); // Output: true
+console.log(notFunktion(true));  // Output: false */
+
+
+
+/*function timerFunktion(startcountdown) {   //Timer Funktion, countdown in Sekund
+    let countdown = startcountdown;
+    console.log('Timer started with countdown: ' + countdown);
+    let current = countdown;
+    const interval = setInterval(() => {
+        console.log(current);
+        current--;
+        if (current < 0) {
+            clearInterval(interval);
+            console.log('Timer finished after ' + countdown + ' seconds.');
+            if (current <0) {
+                console.log('Countdown is below zero! Thanks for waiting! :)');
+            }
+        }
+    }, 1000);
+}
+
+countdown = 5; // Set the countdown time in seconds
+timerFunktion(countdown); */
+
+
+
+
+
+/*function countToTen() {
+    for (let i = 0; i <= 10; i++) {
+        console.log(i);
+    }
+}
+countToTen();*/
+
+
+
+
+
+
+
+
+
+
+
+
+
