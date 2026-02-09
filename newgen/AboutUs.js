11c55bec5936cf370ab1db4cbdec8feffe08ea51
Oscar <ogge9909vs@hotmail.com>
2026-02-09 14:14:03 +0100

    Randomizer Game

---
+++ newgen/AboutUs.js
@@ -0,0 +1,79 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const randomizerGame = document.querySelector('.randomizer-game');
+    const randomizeBtn = document.getElementById('randomize-btn');
+    const animeResult = document.getElementById('anime-result');
+
+
+
+ if (randomizeBtn) {
+    randomizeBtn.addEventListener('click', function() {
+        const animeList = [ // listan av alternativen för randomizer
+            "Attack on Titan",
+            "Demon Slayer",
+            "My Hero Academia",
+            "Jujutsu Kaisen",
+            "One Piece",
+            "Naruto",
+            "Bleach",
+            "Dragon Ball Z",
+            "Hells Paradise",
+            "Tokyo Revengers",
+            "Fullmetal Alchemist: Brotherhood",
+            "Death Note",
+            "Sword Art Online",
+            "One Punch Man",
+            "Mob Psycho 100",
+            "Black Clover",
+            "Fairy Tail",
+            "Haikyuu!!",
+            "Blue Lock",
+            "Mashle: Magic and Muscles",
+            "Fire Force",
+            "Spy x Family",
+            "Berserk",
+            "Vinland Saga",
+            "Akame ga Kill!",  
+            "The Rising of the Shield Hero",
+            "Made in Abyss",
+            "The Promised Neverland",
+            "Dr. Stone",
+            "Cyberpunk: Edgerunners",
+            "Chainsaw Man",
+            "Tokyo Ghoul",
+            "Parasyte: The Maxim",
+            "Erased",
+            "Hunter X Hunter",
+            "Gachiakuta",
+            "Oshi no Ko",
+            "Frieren: Beyond Journey's End",
+            "Blue Period",
+            "Dorohedoro",
+            "Kingdom",
+            "Tougen Anki",
+            "Fate Strange Fake",
+            "MF Ghost",
+            "Kaguya-sama: Love is War",
+            "The Quintessential Quintuplets",
+            "Mushuko Tensei: Jobless Reincarnation",
+            "The Ancient Magus' Bride",
+            "86: Eighty-Six",
+            "The Devil is a Part-Timer!",
+            "Re:Zero - Starting Life in Another World",
+            "My dress-up darling",
+            "Horimiya",
+            "The apothecary diaries",
+            "Kaiju No. 8",
+            "Sword Art Online",
+            "DAN DA DAN",
+            "The Eminence in Shadow",
+            "Assassination Classroom"
+
+
+        ];
+        const randomAnime = animeList[Math.floor(Math.random() * animeList.length)];
+        animeResult.textContent = randomAnime; //random anime visas i resultatdiven
+    });
+ }
+
+
+});
